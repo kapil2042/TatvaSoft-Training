94cbb464221b81871cf3293e84d0ff3eda6deeca
kapil2042 <kapilpatel2042@gmail.com>
2023-02-10 16:37:23 +0530

    change login,registration,forgot password page into mvc view page

---
+++ MVC/CI-Platform/CI-PlatformWeb/Areas/User/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_PlatformWeb.Areas.User.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ForgotPass()
+        {
+            return View();
+        }
+        public IActionResult ResetPass()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
