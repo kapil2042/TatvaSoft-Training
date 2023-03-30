c8f9aadee73c5c4f79cb551548ff12b30fbc7f86
kapil2042 <kapilpatel2042@gmail.com>
2023-03-30 12:35:19 +0530

    User Profile Design

---
+++ MVC/CI-Platform/CI-PlatformWeb/Areas/Volunteer/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_PlatformWeb.Areas.Volunteer.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+    }
+}
