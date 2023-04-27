0dc3c19a233f711e1a134abec1e13242c97e4c8a
kapil2042 <kapilpatel2042@gmail.com>
2023-04-27 11:17:17 +0530

    Make some changes

--- MVC/CI-Platform/CI-Platform.Models/ViewModels/VMUserRegistration.cs
+++ MVC/CI-Platform/CI-Platform.Models/ViewModels/VMUserRegistration.cs
@@ -9,26 +9,28 @@ namespace CI_Platform.Models.ViewModels
 {
     public class VMUserRegistration
     {
-        [Required(ErrorMessage ="First Name is Required")]
-        public string FirstName { get; set; }
+        [Required(ErrorMessage = "First Name is Required")]
+        public string FirstName { get; set; } = null!;
 
         [Required(ErrorMessage = "Last Name is Required")]
-        public string LastName { get; set; }
+        public string LastName { get; set; } = null!;
 
         [Required(ErrorMessage = "Email is Required")]
         [EmailAddress(ErrorMessage = "Invalid Email Address")]
-        public string Email { get; set; }
+        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; } = null!;
 
         [Required(ErrorMessage = "Mobile Number is Required")]
-        public string MobileNo { get; set; }
+        [RegularExpression(@"(\+[0-9]{3})?[-.]?[0-9]{10}|\+([0-9]{2})?[-.]?[0-9]{10}|(\+[0-9]{1})?[-.]?[0-9]{10}$",ErrorMessage = "Plz enter valid Phone Number ex. '9876543210', '+9876543210','+91-9876543210'")]
+        public string MobileNo { get; set; } = null!;
 
         [Required(ErrorMessage = "Password is Required")]
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string Password { get; set; } = null!;
 
         [Required(ErrorMessage = "Confirm Password is Required")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage ="Password and Confirm Password Not Match")]
-        public string CnfPassword { get; set; }
+        public string CnfPassword { get; set; } = null!;
     }
 }
